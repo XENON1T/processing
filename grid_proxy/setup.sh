bab9542ee3d165baba39e442cbe3b5f6e3e456a9
Patrick de Perio <pdeperio@astro.columbia.edu>
2018-08-06 15:28:46 -0500

    Update OSG client version from 3.3 to 3.4

--- grid_proxy/setup.sh
+++ grid_proxy/setup.sh
@@ -1,1 +1,1 @@
-source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.3/current/el6-x86_64/setup.sh
+source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.4/current/el6-x86_64/setup.sh
